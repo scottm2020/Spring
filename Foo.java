bae5ce5d3f62a11d6ae20cef4326d4748680aa63
scottm2020 <scottm@adobe.com>
2019-10-16 20:20:19 -0400

    Add files via upload

---
+++ Foo.java
@@ -0,0 +1,17 @@
+package com.apress.springrecipes.sequence;
+
+public class Foo {
+
+    private String msg;
+
+    public void SetMsg(String msg)
+    {
+        this.msg = msg;
+    }
+
+    public String getMsg()
+    {
+        return this.msg ;
+    }
+
+}
